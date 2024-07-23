6ee9eba10bc5a4a4f5e77a2d7e5ec07c7313734b
edd-ie <ogendoeddie@gmail.com>
2024-07-23 18:41:53 -0300

    AAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- app/src/main/java/edd_ie/com/github/arnav/MainActivity.kt
+++ app/src/main/java/edd_ie/com/github/arnav/MainActivity.kt
@@ -2,6 +2,10 @@ package edd_ie.com.github.arnav
 
 
 import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CaptureRequest
+import android.hardware.camera2.TotalCaptureResult
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -15,7 +19,11 @@ import com.google.ar.core.CameraConfigFilter
 import com.google.ar.core.Config
 import com.google.ar.core.Session
 import com.google.ar.core.SharedCamera
+import com.google.ar.core.exceptions.CameraNotAvailableException
+import com.google.ar.core.exceptions.UnavailableApkTooOldException
 import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException
 import edd_ie.com.github.arnav.databinding.ActivityMainBinding
 import java.util.EnumSet
 
@@ -26,11 +34,14 @@ class MainActivity : AppCompatActivity() {
 
     // requestInstall(Activity, true) will triggers installation of
     // Google Play Services for AR if necessary.
+    lateinit var arCoreSessionHelper: ARCoreSessionLifecycleHelper
+
+    val instantPlacementSettings = InstantPlacementSettings()
+    val depthSettings = DepthSettings()
+    val showCamera:ShowCamera= ShowCamera();
+
     private var userRequestedInstall = true
     private var session: Session? = null
-    private var sharedSession: Session? = null
-    private var sharedCamera: SharedCamera? = null
-    private var cameraId: String? = null
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,13 +49,33 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
 
-        enableEdgeToEdge()
-        setContentView(view)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        // Setup ARCore session lifecycle helper and configuration.
+        arCoreSessionHelper = ARCoreSessionLifecycleHelper(this)
+        // If Session creation or Session.resume() fails, display a message and log detailed
+        // information.
+        arCoreSessionHelper.exceptionCallback =
+            { exception ->
+                val message =
+                    when (exception) {
+                        is UnavailableUserDeclinedInstallationException ->
+                            "Please install Google Play Services for AR"
+                        is UnavailableApkTooOldException -> "Please update ARCore"
+                        is UnavailableSdkTooOldException -> "Please update this app"
+                        is UnavailableDeviceNotCompatibleException -> "This device does not support AR"
+                        is CameraNotAvailableException -> "Camera not available. Try restarting the app."
+                        else -> "Failed to create AR session: $exception"
+                    }
+                Log.e(TAG, "ARCore threw an exception", exception)
+            }
+
+        // Configure session features, including: Lighting Estimation, Depth mode, Instant Placement.
+        arCoreSessionHelper.beforeSessionResume = ::configureSession
+        lifecycle.addObserver(arCoreSessionHelper)
+
+
+
+        depthSettings.onCreate(this)
+        instantPlacementSettings.onCreate(this)
 
     }
 
@@ -93,6 +124,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // Configure the session, using Lighting Estimation, and Depth mode.
+    fun configureSession(session: Session) {
+        session.configure(
+            session.config.apply {
+                lightEstimationMode = Config.LightEstimationMode.ENVIRONMENTAL_HDR
+
+                // Depth API is used if it is configured in Hello AR's settings.
+                depthMode =
+                    if (session.isDepthModeSupported(Config.DepthMode.AUTOMATIC)) {
+                        Config.DepthMode.AUTOMATIC
+                    } else {
+                        Config.DepthMode.DISABLED
+                    }
+
+                // Instant Placement is used if it is configured in Hello AR's settings.
+                instantPlacementMode =
+                    if (instantPlacementSettings.isInstantPlacementEnabled) {
+                        Config.InstantPlacementMode.LOCAL_Y_UP
+                    } else {
+                        Config.InstantPlacementMode.DISABLED
+                    }
+            }
+        )
+    }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
@@ -111,35 +167,12 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+
+
     @SuppressLint("SetTextI18n")
     override fun onResume() {
         super.onResume()
-
-        // Check camera permission.
-        // ARCore requires camera permission to operate.
-        if (!CameraPermissionHelper.hasCameraPermission(this)) {
-            CameraPermissionHelper.requestCameraPermission(this)
-            return
-        }
-
-        // Ensure that Google Play Services for AR and ARCore device profile data are
-        // installed and up to date.
-        // Enable AR-related functionality on ARCore supported devices only.
-        arSupportCheck()
-
-
-        // Create an ARCore session that supports camera sharing.
-        sharedSession = Session(this, EnumSet.of(Session.Feature.SHARED_CAMERA))
-
-        // Store the ARCore shared camera reference.
-        sharedCamera = sharedSession!!.sharedCamera
-
-        // Store the ID of the camera that ARCore uses.
-        cameraId = sharedSession!!.cameraConfig.cameraId
-
-
-        //Initializing ar session
-        createArSession()
+        showCamera.showCamera()
     }
 
     override fun onDestroy() {
@@ -147,38 +180,10 @@ class MainActivity : AppCompatActivity() {
         session?.close()
     }
 
-    private fun createArSession() {
-        // Create a session config.
-        val config = Config(session)
 
-        // Do feature-specific operations here, such as enabling depth or turning on
-        // support for Augmented Faces.
-        // Create a camera config filter for the session.
-
-        val filter = CameraConfigFilter(session)
-
-        // Return only camera configs that target 30 fps camera capture frame rate.
-        filter.targetFps = EnumSet.of(CameraConfig.TargetFps.TARGET_FPS_30)
-
-        // Get list of configs that match filter settings.
-        // In this case, this list is guaranteed to contain at least one element,
-        // because both TargetFps.TARGET_FPS_30 and DepthSensorUsage.DO_NOT_USE
-        // are supported on all ARCore supported devices.
-        val cameraConfigList = session?.getSupportedCameraConfigs(filter)
-        if (cameraConfigList != null) {
-            for(x in cameraConfigList){
-                println(x)
-            }
-        }
-
-        // Use element 0 from the list of returned camera configs. This is because
-        // it contains the camera config that best matches the specified filter
-        // settings.
-        session?.cameraConfig = cameraConfigList?.get(0)!!
-
-
-        // Configure the session.
-        session?.configure(config)
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        FullScreenHelper.setFullScreenOnWindowFocusChanged(this, hasFocus)
     }
 }
 
